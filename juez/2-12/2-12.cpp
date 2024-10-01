d9755899c022c68ab7e4b034b804b92ec7f88eb7
cyntrist <cyntrist@ucm.es>
2024-10-01 15:47:13 +0200

    2.12 y 2.13

--- juez/2-12/2-12.cpp
+++ juez/2-12/2-12.cpp
@@ -1,19 +1,64 @@
-// 2-12.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// Nombre del alumno ..... Cynthia Tristán
+// Usuario del Juez ...... EDA-GDV73
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// función que resuelve el problema
+// O(log n) siendo n el tamaño del primer vector
+// esto es porque es una búsqueda binaria
+int resolver(vector<int> const& v1, vector<int> const& v2, int ini, int fin) {
+    int n = fin - ini;
+
+	if (n == 0) // vector nulo 
+		return ini;
+    if (v1[ini] != v2[ini]) // comparaciono de primeros
+        return ini;
+	if (n == 1) // inmediato a la derecha
+		return ini + 1;
+
+    int mit = (ini + fin) / 2;
+
+    if (v1[mit] == v2[mit]) // si es correcto busca a la derecha
+		return resolver(v1, v2, mit, fin);
+		
+	return resolver(v1, v2, ini, mit);
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso() {
+    long long int numElementos = 0;
+    cin >> numElementos;
+    vector<int> v1(numElementos);
+    vector<int> v2(numElementos - 1);
+    for (int& e : v1) cin >> e;
+    for (int& e : v2) cin >> e;
+    int sol = resolver(v1, v2, 0, v1.size()-1);
+    cout << v1[sol] << endl;
+}
+
+int main() {
+    // Para la entrada por fichero.
+    // Comentar para acepta el reto
+#ifndef DOMJUDGE
+    ifstream in("datos.txt");
+    auto cinbuf = cin.rdbuf(in.rdbuf()); //save old buf and redirect cin to casos.txt
+#endif
+
+    int numCasos;
+    cin >> numCasos;
+    for (int i = 0; i < numCasos; ++i)
+        resuelveCaso();
+
+    // Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+    cin.rdbuf(cinbuf);
+#endif
+
+    return 0;
+}
--- juez/2-14/2-13.cpp
+++ juez/2-14/2-13.cpp
@@ -1,19 +1,77 @@
-// 2-14.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+// Nombre del alumno ..... Cynthia Tristán
+// Usuario del Juez ...... EDA-GDV73
 
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+using namespace std;
 
-int main()
+// función que resuelve el problema
+// O(log n) siendo n el tamaño del vector
+// esto es porque es una búsqueda binaria
+int resolver(vector<int>& v, int ini, int fin)
 {
-    std::cout << "Hello World!\n";
+	const int n = fin - ini;
+
+	if (n == 0) // seccion nula
+		return ini;
+	if (n == 1) // inmediato
+		return ini;
+
+	const int mit = (ini + fin) / 2;
+
+	if (mit % 2 == 0) // si el elto en la mitad esta en una pos par
+	{
+		if (v[mit] == v[mit - 1]) // si es igual al de su izq 
+			return resolver(v, ini, mit); // esta en la izq
+		return resolver(v, mit, fin); // si no esta a la derecha
+	}
+
+	// si esta en una posicion impar
+	if (v[mit] == v[mit - 1]) // si es igual a su izq
+		return resolver(v, mit, fin); // esta a la derecha
+	return resolver(v, ini, mit); // si no, esta a la izq
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+// Resuelve un caso de prueba, leyendo de la entrada la
+// configuración, y escribiendo la respuesta
+void resuelveCaso()
+{
+	// leer los datos de la entrada
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int& e : v) cin >> e;
+
+	// Llamada a la función resolver
+	cout << resolver(v, 0, n) << endl;
+	// Mostrar el resultado
+}
+
+
+//#define DOMJUDGE
+int main()
+{
+	// Para la entrada por fichero.
+	// Comentar para acepta el reto
+#ifndef DOMJUDGE
+	std::ifstream in("datos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+#endif
+
+	int numCasos;
+	std::cin >> numCasos;
+	for (int i = 0; i < numCasos; ++i)
+		resuelveCaso();
+
+
+	// Para restablecer entrada. Comentar para acepta el reto
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	//system("PAUSE");
+#endif
+
+	return 0;
+}
